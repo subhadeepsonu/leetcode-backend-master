b9071d182b7efbee124aa912a6a6e77843409c1d
subhadeepsonu <subhadeepthandaka@gmail.com>
2025-01-05 21:55:03 +0530

    sefg

--- index.ts
+++ index.ts
@@ -29,7 +29,15 @@ const allowedOrigins = [
 
 
 app.use(cors({
-    origin: "*"
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json())
 app.options('*', cors());
