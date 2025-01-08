11d89227095ff85b80594858488ba19e541ff504
subhadeepsonu <subhadeepthandaka@gmail.com>
2025-01-08 09:37:50 +0530

    local access

--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: "https://meetcode-user.subhadeep.xyz"
+    origin: ["https://meetcode-user.subhadeep.xyz", "http://localhost:5173"]
 }));
 app.use(express.json())
 
